cc8a91eac86412534b5269ab92238e49673024eb
practizer <herostack777@gmail.com>
2026-03-01 22:02:29 +0530

    added frontend url in cors middleware

--- server/main.go
+++ server/main.go
@@ -10,7 +10,7 @@ import (
 func main(){
 	router := gin.Default()	
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173","https://random-song-singer.vercel.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
